ff58adcd0294e6ae862eaf4b62fbcfc7e7a0c58a
Aryan <112301041@smail.iitpkd.ac.in>
2026-08-11 18:56:53 +0530

    assigned each vertex a diff color

--- Lab2-Triangle/triangle.cpp
+++ Lab2-Triangle/triangle.cpp
@@ -74,9 +74,9 @@ int main(){
     
     
     float vertices[] = {
-        -0.5f,-0.5f,0.0f,
-        0.5f,-0.5f,0.0f,
-        0.0f,0.5f,0.0f
+        -0.5f,-0.5f,0.0f, 1.0f,0.0f,0.0f,
+        0.5f,-0.5f,0.0f,  0.0f,1.0f,0.0f,
+        0.0f,0.5f,0.0f,   0.0f,0.0f,1.0f
     };
 
 
@@ -124,8 +124,10 @@ int main(){
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
-    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,3*sizeof(float),(void*)0);
+    glVertexAttribPointer(0,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)0);
+    glVertexAttribPointer(1,3,GL_FLOAT,GL_FALSE,6*sizeof(float),(void*)(3*sizeof(float)));
     glEnableVertexAttribArray(0);
+    glEnableVertexAttribArray(1);
 
     while(!glfwWindowShouldClose(window)){
         glClearColor(0.1f,0.1f,0.1f,1.0f);
